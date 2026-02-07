626da54d7cca9cf43ec585c115a447195ba40cc4
anton <anton@LAPTOP-B1GN23L2>
2026-02-07 15:47:21 +0200

    Fix mni player8

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,16 @@ import App from "./App";
 ReactDOM.createRoot(
   document.getElementById("root")!
 ).render(
-  
-    <App />
- 
+  <App />
 );
+
+// register service worker
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js");
+  });
+}
+
+// remove splash screen
+const splash = document.getElementById("splash");
+if (splash) splash.remove();
